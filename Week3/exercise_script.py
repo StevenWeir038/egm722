b0b85bf50ae9ffbe04917ab65cf1dfa46dc02dc2
StevenWeir038 <87386761+StevenWeir038@users.noreply.github.com>
2026-03-29 21:09:53 +0100

    experiment with objects
    just playing with objects and getting to understand libraries and parameters

--- Week3/exercise_script.py
+++ Week3/exercise_script.py
@@ -1,3 +1,4 @@
+# imports
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -12,26 +13,71 @@ import matplotlib.patches as mpatches
 
 # load the necessary data here and transform to a UTM projection
 
+# 1. Load the counties and ward data
+counties = gpd.read_file('data_files/counties.shp')
+wards = gpd.read_file('data_files/NI_wards.shp')
+
+# 2. Transform loaded data to same relevant UTM projection (Ireland UTM29 = espg2157)
+counties_utm = counties.to_crs(2157)
+wards_utm = wards.to_crs(2157)
+
+# 3. Create a spatial join
+join = gpd.sjoin(counties_utm, wards_utm,how='inner', lsuffix='left', rsuffix='right')
+
 # your analysis goes here...
+# 4. Summarize the total population by county to understand:
+#       What county has the highest population?
+#       What county has the lowest population?
+#       (N.B. there are many wards that extend over county boundaries; for this exercise only, ignore that problem.)
+group_counties_population = join.groupby(['CountyName', 'Population'])
+county_population_total = join['Population'].sum()
 
 # ---------------------------------------------------------------------------------------------------------------------
 # below here, you may need to modify the script somewhat to create your map.
+
+# create helper function for legend and map name features
+def generate_handles(labels, colors, edge='k', alpha=1):
+    """
+    Generate matplotlib patch handles to create a legend of each of the features in the map.
+
+    Parameters
+    ----------
+
+    labels : list(str)
+        the text labels of the features to add to the legend
+
+    colors : list(matplotlib color)
+        the colors used for each of the features included in the map.
+
+    edge : matplotlib color (default: 'k')
+        the color to use for the edge of the legend patches.
+
+    alpha : float (default: 1.0)
+        the alpha value to use for the legend patches.
+
+    Returns
+    -------
+
+    handles : list(matplotlib.patches.Rectangle)
+        the list of legend patches to pass to ax.legend()
+    """
+    lc = len(colors)  # get the length of the color list
+    handles = [] # create an empty list
+    for ii in range(len(labels)): # for each label and color pair that we're given, make an empty box to pass to our legend
+        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[ii % lc], edgecolor=edge, alpha=alpha))
+    return handles
+
 # create a crs using ccrs.UTM() that corresponds to our CRS
 ni_utm = ccrs.UTM(29)
 
 # create a figure of size 10x10 (representing the page size in inches
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=ni_utm))
 
-# add gridlines below
-gridlines = ax.gridlines(draw_labels=True,
-                         xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],
-                         ylocs=[54, 54.5, 55, 55.5])
-gridlines.right_labels = False
-gridlines.bottom_labels = False
+
 
 # to make a nice colorbar that stays in line with our map, use these lines:
 divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
+cax = divider.append_axes("right", size="3%", pad=0.1, axes_class=plt.Axes)
 
 # plot the ward data into our axis, using gdf.plot()
 ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
@@ -41,10 +87,18 @@ ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='v
 county_outlines = ShapelyFeature(counties['geometry'], ni_utm, edgecolor='r', facecolor='none')
 ax.add_feature(county_outlines)
 
+# add gridlines
+ax = plt.axes(projection=ni_utm)
+gridlines = ax.gridlines(draw_labels=True, linewidth=0.5, linestyle='-', color='gray', # draw  labels for the grid lines
+                         xlocs=[-8, -7.5, -7, -6.5, -6, -5.5], # add longitude lines at 0.5 deg intervals
+                         ylocs=[54, 54.5, 55, 55.5]) # add latitude lines at 0.5 deg intervals
+ax.set_extent([-6, -5, 54, 55], crs=ccrs.PlateCarree())
+gridlines.right_labels = False
+gridlines.bottom_labels = False
 county_handles = generate_handles([''], ['none'], edge='r')
 
 # add a legend in the upper left-hand corner
-ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
+ax.legend(county_handles, ['County Boundaries'], fontsize=8, loc='upper left', framealpha=1)
 
 # save the figure
 fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
